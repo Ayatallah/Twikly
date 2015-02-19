e9177dba5a39245cc8ab6ea396fac458ec4bc34f
Tarek ElBeih <ispoonjelly@Mr.kenedy>
2015-02-19 18:10:01 +0200

    added user authentication to blog

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-	
+	before_action :authenticate_user!
+
 	def index
 		@articles = Article.all.reverse
 	end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219140925) do
+ActiveRecord::Schema.define(version: 20150219152152) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,26 @@ ActiveRecord::Schema.define(version: 20150219140925) do
   end
 
   add_index "sub_comments", ["comment_id"], name: "index_sub_comments_on_comment_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
+    t.boolean  "auth",                   default: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "youtubes", force: :cascade do |t|
     t.string   "youtube_url"
