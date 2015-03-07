12d2f92f0e9bd5c90e208c23c0b4b8cd417340b5
Tarek ElBeih <ispoonjelly@Mr.kenedy>
2015-03-07 17:39:42 +0200

    init

---
+++ Looprac/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create!(title: 'shopping', notes: 'tomato, tomato, tomato')
+Todo.create!(title: 'steal the bike')
+Todo.create!(title: 'Get to Diamond1', notes: 'pick orianna -> win')
+Todo.create!(title: 'google maps api', notes: 'you have to')
+Todo.create!(title: 'JS!', notes: 'just do it')
