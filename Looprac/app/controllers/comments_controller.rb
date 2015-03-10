8da185af86bfc36045e649b3fcc6e7bc9d0ac50e
Ahmed Abdel-Hamid <a_walid@live.com>
2015-03-10 17:30:53 +0200

    Deleting comments succesfully

--- Looprac/app/controllers/comments_controller.rb
+++ Looprac/app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to post_path(@post)
   end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
  
   private
     def comment_params
--- Looprac/app/models/post.rb
+++ Looprac/app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
 	validates :title, presence: true,
                     length: { minimum: 5 }
-
+     
     acts_as_votable
-    has_many :comments
+    has_many :comments, dependent: :destroy
     belongs_to :user
 end
