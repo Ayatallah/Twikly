17334e2902096c342200f4fc32280186346e1204
ahmedtarek <ahmed.t.abdelfattah@gmail.com>
2015-03-11 23:05:41 +0200

    Admin view

--- Looprac/app/controllers/comments_controller.rb
+++ Looprac/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
 	def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
